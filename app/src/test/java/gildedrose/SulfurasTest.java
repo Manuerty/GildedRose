33aecb568d4747ac50f4ecc0e533f6e080957655
Manuerty <manuelcendonr@gmail.com>
2024-05-02 12:47:58 +0200

    test (Sulfuras): create test for  Sulfuras.java

---
+++ app/src/test/java/gildedrose/SulfurasTest.java
@@ -0,0 +1,44 @@
+package gildedrose;
+
+import gildedrose.Item.Sulfuras;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SulfurasTest {
+
+    @Test
+    public void qualityRemainsEightyAfterUpdate() {
+        Sulfuras sulfuras = new Sulfuras("Sulfuras", 5, 10);
+        sulfuras.updateItem();
+        assertEquals(80, sulfuras.getQuality());
+    }
+
+    @Test
+    public void sellInRemainsUnchangedAfterUpdate() {
+        Sulfuras sulfuras = new Sulfuras("Sulfuras", 5, 10);
+        sulfuras.updateItem();
+        assertEquals(5, sulfuras.getSellIn());
+    }
+
+    @Test
+    public void qualityRemainsEightyWhenQualityIsZero() {
+        Sulfuras sulfuras = new Sulfuras("Sulfuras", 5, 0);
+        sulfuras.updateItem();
+        assertEquals(80, sulfuras.getQuality());
+    }
+
+    @Test
+    public void qualityRemainsEightyWhenQualityIsEighty() {
+        Sulfuras sulfuras = new Sulfuras("Sulfuras", 5, 80);
+        sulfuras.updateItem();
+        assertEquals(80, sulfuras.getQuality());
+    }
+
+    @Test
+    public void sellInRemainsNegativeAfterUpdate() {
+        Sulfuras sulfuras = new Sulfuras("Sulfuras", -1, 10);
+        sulfuras.updateItem();
+        assertEquals(-1, sulfuras.getSellIn());
+    }
+}
