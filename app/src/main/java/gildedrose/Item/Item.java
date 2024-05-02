b6d6201af53fb829d6eac513f29470d2f22d666e
Manuerty <manuelcendonr@gmail.com>
2024-05-02 11:05:24 +0200

    feat (Item.Java): Create Abstract Class Item

---
+++ app/src/main/java/gildedrose/Item/Item.java
@@ -0,0 +1,20 @@
+package gildedrose.Item;
+
+public abstract class Item implements Updatable {
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
