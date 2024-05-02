37d6b766757fe69c55257c058e72fd57bb26c697
Manuerty <manuelcendonr@gmail.com>
2024-05-02 12:29:15 +0200

    test (AgedBrie): Create Test for AgedBrie.java

---
+++ app/src/test/java/gildedrose/AgedBrieTest.java
@@ -0,0 +1,36 @@
+package gildedrose;
+
+import gildedrose.Item.AgedBrie;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AgedBrieTest {
+    @Test
+    public void qualityRemainsUnchangedWhenQualityIsFifty() {
+        AgedBrie agedBrie = new AgedBrie("Aged Brie", 5, 50);
+        agedBrie.updateItem();
+        assertEquals(50, agedBrie.getQuality());
+    }
+
+    @Test
+    public void qualityIncreasesByTwoWhenSellInIsNegative() {
+        AgedBrie agedBrie = new AgedBrie("Aged Brie", -1, 10);
+        agedBrie.updateItem();
+        assertEquals(12, agedBrie.getQuality());
+    }
+
+    @Test
+    public void sellInBecomesNegativeAfterUpdate() {
+        AgedBrie agedBrie = new AgedBrie("Aged Brie", 0, 10);
+        agedBrie.updateItem();
+        assertEquals(-1, agedBrie.getSellIn());
+    }
+
+    @Test
+    public void qualityIncreasesByOneWhenQualityIsZeroAndSellInIsPositive() {
+        AgedBrie agedBrie = new AgedBrie("Aged Brie", 5, 0);
+        agedBrie.updateItem();
+        assertEquals(1, agedBrie.getQuality());
+    }
+}
