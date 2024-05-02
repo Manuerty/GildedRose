31425550d286efc8eb07aab88b89989e667f134a
Manuerty <manuelcendonr@gmail.com>
2024-05-02 12:03:05 +0200

    feat (GildedRose.Java): Create clas GildedRose.java

---
+++ app/src/main/java/gildedrose/GildedRose.java
@@ -0,0 +1,16 @@
+package gildedrose;
+
+import java.util.Arrays;
+import gildedrose.Item.Item;
+
+public class GildedRose {
+    public Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        Arrays.stream(items).forEach(Item::updateQuality);
+    }
+}
